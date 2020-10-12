c91b1c2568d9faeec91290ba82883628801613c8
Dong-Yi-Xia <dong7777xia@gmail.com>
2020-10-12 12:04:22 -0400

    finish del

--- src/components/AccountContainer.js
+++ src/components/AccountContainer.js
@@ -4,12 +4,59 @@ import Search from "./Search";
 import AddTransactionForm from "./AddTransactionForm";
 
 class AccountContainer extends Component {
+
+  state = {
+    transactions : [],
+    search: ""
+  } 
+
+  componentDidMount(){
+    fetch('http://localhost:6001/transactions')
+    .then(r => r.json())
+    .then(resp => {
+      this.setState({
+        transactions: resp
+      })
+    })
+  }
+
+
+  addTransactionFun = (addTransaction) =>{
+    let newTransArr = [...this.state.transactions, addTransaction]
+    this.setState({
+      transactions: newTransArr
+    })
+  }
+
+  searchFun = (searchResult) => {
+    this.setState({
+      search: searchResult
+    })
+  }
+
+  filterSearchTransactions = () => {
+    let {transactions, search} = this.state
+    let filterSearch = transactions.filter(transaction => {
+      return transaction.description.toLowerCase().includes(search.toLowerCase())
+    })
+    return filterSearch
+  }
+
   render() {
     return (
       <div>
-        <Search />
-        <AddTransactionForm />
-        <TransactionsList />
+        <Search 
+           searchValue={this.state.search}
+           searchFun={this.searchFun}
+        />
+
+        <AddTransactionForm 
+          addTransactionFun={this.addTransactionFun}
+        />
+
+        <TransactionsList
+          transactions={this.filterSearchTransactions()}
+         />
       </div>
     );
   }
--- src/components/TransactionsList.js
+++ src/components/TransactionsList.js
@@ -1,7 +1,12 @@
 import React from "react";
 import Transaction from "./Transaction";
 
-const TransactionsList = () => {
+const TransactionsList = (props) => {
+
+  let componentArray = props.transactions.map(transactionObj => {
+    return <Transaction key={transactionObj.id} transaction={transactionObj}/>
+  })
+
   return (
     <table className="ui celled striped padded table">
       <tbody>
@@ -19,7 +24,7 @@ const TransactionsList = () => {
             <h3 className="ui center aligned header">Amount</h3>
           </th>
         </tr>
-        {/* render Transactions here */}
+        {componentArray}
       </tbody>
     </table>
   );
